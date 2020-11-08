c199c246e21a43229c40f650fb9a01cf3e66c2b7
Evgeniy Khaberev <ehaberev@yandex.ru>
2020-11-08 14:45:25 +0500

    Add using ES6 Class example for Lesson-2

--- Lesson-2/Examples/JS-Basics/4-objects.js
+++ Lesson-2/Examples/JS-Basics/4-objects.js
@@ -1,3 +1,5 @@
+// Define object as instance (of "anonymous" class)
+
 const person = {
   name: 'Evgeniy',
   age: 36,
@@ -9,16 +11,35 @@ const person = {
   }
 };
 
-const Person = function(name, age, hasHobbies) {
-  this.name = name;
-  this.age = age;
-  this.hasHobbies = hasHobbies;
-  this.greet = function() {
-    console.log('Hi, I am ' + this.name);
-  }
-};
-
 person.greet();
 
-anotherPerson = new Person('Alexander', 30, true);
+// Define class as function
+
+// const Person = function(name, age, hasHobbies) {
+//   this.name = name;
+//   this.age = age;
+//   this.hasHobbies = hasHobbies;
+//   this.greet = function() {
+//     console.log('Hi, I am ' + this.name);
+//   }
+// };
+
+// anotherPerson = new Person('Alexander', 30, true);
+// anotherPerson.greet();
+
+// Define class as ES6 Class
+
+class Person {
+  constructor(name, age, hasHobbies) {
+    this.name = name;
+    this.age = age;
+    this.hasHobbies = hasHobbies;
+  }
+
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+}
+
+anotherPerson = new Person('Petr', 32, true);
 anotherPerson.greet();
